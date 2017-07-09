aa05a00c081c74ad8f14c65eb623b98316e7867b
PaphaC <oxachon@hotmail.fr>
2017-07-09 17:47:37 +0200

    feat: added animations

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from apps.reader import json_parser
 from apps.event import event_handler
 from apps.audio import audio_handler
 from apps.display import print_display
+from apps.display import mouth_movement
+import time
+clock = pygame.time.Clock()
+FPS = 120
+frames = FPS / 12
 
 # Defining start file
 current_file = 'system/json_example.json'
@@ -11,46 +16,71 @@ current_file = 'system/json_example.json'
 pygame.init()
 
 # Creating our window
-window = pygame.display.set_mode((1280, 720))
+window = pygame.display.set_mode((645, 365))
+pygame.display.set_caption('VA-11 HALL-A')
 
-# Init static vars
+# Init global vars
 previousSound = ''
-myfont = pygame.font.SysFont("arial", 20)
-ui = pygame.image.load("media/ui/messagebox.png")
+previousName = ''
+previousSprite = ''
+myfont = pygame.font.SysFont("arial", 15)
+ui = pygame.image.load("media/ui/interface.png")
+bg = pygame.image.load("media/ui/dynamic_bg.png")
+icon = pygame.image.load("media/ui/icon.png")
+low_voice = pygame.mixer.Sound("media/sfx/bleep_low.ogg")
+low_voice.set_volume(.2)
+pygame.display.set_icon(icon)
 
 is_start = 1
 score = 0
 i = 0
 j = 0
+c_m_sprite = 0
 # Parsing first default file
 json_obj = json_parser(current_file)
 
 # Main loop
 while is_start:
     # Song load
-    if (json_obj['scenes'][i]['music'] != '' or json_obj['scenes'][i]['music'] is not None) \
-            and previousSound != json_obj['scenes'][i]['music']:
+    if 'music' in json_obj['scenes'][i] and previousSound != json_obj['scenes'][i]['music']:
         audio_handler(json_obj['scenes'][i]['music'], pygame)
         previousSound = json_obj['scenes'][i]['music']
+
+    # Setting current phrase length
     current_phrase_len = len(json_obj['scenes'][i]['text'])
+
     # Event handling in here
     value_event = event_handler(pygame.event.get())
     if value_event == 'QUIT':
         is_start = 0
     if value_event == 'LEFT_CLICK':
-        if i < len(json_obj['scenes'])-1:
+        if i < (len(json_obj['scenes']) - 1):
             if j < current_phrase_len:
                 j = current_phrase_len
             else:
                 i += 1
                 j = 0
-        elif json_obj['next_file'] is not None and i == len(json_obj['scenes'])-1:
+        elif 'next_file' in json_obj and i == len(json_obj['scenes']) - 1:
             i = 0
             j = 0
-            json_obj = json_parser('system/'+json_obj['next_file'])
+            json_obj = json_parser('system/' + json_obj['next_file'])
 
     # Display game
     if j < current_phrase_len:
-        j += 1
-    print_display(window, pygame, json_obj['scenes'][i], ui, myfont, j)
+        j += 2
+        low_voice.play(-1)
+    else:
+        low_voice.stop()
 
+    if 'sprite' in json_obj['scenes'][i]:
+        previousSprite = print_display(window, pygame, json_obj['scenes'][i], bg, ui, myfont, j)
+        if j < current_phrase_len and json_obj['scenes'][i]['name'] != 'Jill':
+            c_m_sprite = mouth_movement(window, pygame, json_obj['scenes'][i]['sprite'], c_m_sprite)
+    else:
+        print_display(window, pygame, json_obj['scenes'][i], bg, ui, myfont, j, previousSprite)
+        if j < current_phrase_len and json_obj['scenes'][i]['name'] != 'Jill':
+            c_m_sprite = mouth_movement(window, pygame, previousSprite, c_m_sprite)
+    time.sleep(.100)
+    clock.tick(FPS)
+
+    # Making the character close his eyes randomly, because why not
--- apps/display.py
+++ apps/display.py
@@ -1,20 +1,65 @@
-def print_display(window, pygame, json_obj, ui, myfont, numchar):
-    # Refilling to empty current space
-    window.fill(pygame.Color("white"))
+import spritesheet
+from random import randint
+import time
+myStruct = {'Jill': (51, 102, 153), 'Alma': (255, 153, 0)}
 
-    # Background first
-    bg = pygame.image.load("media/background/" + json_obj['background'])
-    window.blit(bg, (0, 0))
+
+def print_display(window, pygame, json_obj, bg, ui, myfont, numchar, previousSprite=None):
+    # Avoiding useless JSON repetition and making it clean
+    if 'sprite' in json_obj:
+        current_sprite = json_obj['sprite']
+    else:
+        current_sprite = previousSprite
+
+    # Refilling to empty current space
+    #window.fill(pygame.Color("white"))
 
     # Then comes the character
-    character = pygame.image.load("media/characters/" + json_obj['character'])
-    window.blit(character, (400, 0))
+    character = pygame.image.load("media/characters/" + current_sprite + ".png")
+    window.blit(bg, (10, 16))
+    window.blit(character, (105, 57))
+    window.blit(ui, (0, 0))
 
-    # The UI is the last fo be blitz
-    window.blit(ui, (5, 420))
+    name = json_obj['name']
+    name_lb = myfont.render(name + ":", 1, myStruct[name])
+    window.blit(name_lb, (40, 280))
 
     score = json_obj['text'][0:numchar]
-    label = myfont.render(score, 1, (0, 0, 0))
-    window.blit(label, (100, 560))
+    label = myfont.render(score, 1, (255, 255, 255))
+    window.blit(label, (80, 280))
     # Refreshing the display with all the layers set
+    # Making the character close his eyes randomly, because why not
+    if randint(0, 30) == 5:
+        closing_eyes(window, pygame, current_sprite)
     pygame.display.flip()
+    return current_sprite
+
+
+def closing_eyes(window, pygame, sprite):
+    ss = spritesheet.spritesheet("media/characters/""e_"+sprite+".png")
+    # Sprite is 16x16 pixels at location 0,0 in the file...
+    images = [ss.image_at((0, 0, 61, 28)), ss.image_at((61, 0, 61, 28))]
+    # Load two images into an array, their transparent bit is (255, 255, 255)
+    i = 0
+    while i < len(images):
+        window.blit(images[i], (145, 130))
+        pygame.display.flip()
+        time.sleep(.0500)
+        i += 1
+
+
+def mouth_movement(window, pygame, sprite, i):
+    ss = spritesheet.spritesheet("media/characters/""m_" + sprite + ".png")
+    # Sprite is 16x16 pixels at location 0,0 in the file...
+    images = [ss.image_at((0, 0, 42, 15)),
+              ss.image_at((42, 0, 42, 15)),
+              ss.image_at((84, 0, 42, 15)),
+              ss.image_at((126, 0, 42, 15))]
+    # Load two images into an array, their transparent bit is (255, 255, 255)
+    window.blit(images[i], (152, 170))
+    pygame.display.flip()
+    i += 1
+    if i >= 3:
+        return 0
+    else:
+        return i
